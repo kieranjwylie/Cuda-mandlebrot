4829f6dc50ff3094232cad0cdc365ad696ff1526
Kieran Wylie <kieranjwylie@gmail.com>
2026-06-26 16:33:58 +0100

    Create output.hpp

---
+++ output.hpp
@@ -0,0 +1,6 @@
+#include "data.hpp"
+#include <stdio.h>
+
+int write_mandle(Vertex<double> vertices, std::vector<double> modz);
+
+int write_mandle_bin(Vertex<double> vertices, std::vector<double> modz) ;
